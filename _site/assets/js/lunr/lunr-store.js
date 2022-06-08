df398cba1e9a9a6d5ff399523ecd6c62ee87ab93
ToddMorrill <tmorrill12@gmail.com>
2022-06-07 22:37:40 -0400

    start configuring new theme and add disqus comments

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "EEG Setup",
+        "excerpt":"TLDR; I’m getting started with biometric sensor data, namely EEG. For the last 2 weeks I’ve been spending some time learning about EEG signals and biometrics signals more broadly. EEG signals are an interesting source of data for us because they have the power to tap into what might be...","categories": ["Biometrics"],
+        "tags": [],
+        "url": "/biometrics/eeg-setup/",
+        "teaser": "/assets/images/eeg/eeg_headset.jpg"
+      }]
